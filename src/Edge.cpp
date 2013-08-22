acf495204234d5ec7f44d732d77c0d35259ac03b
ahmadyan <ahmadyan@gmail.com>
2013-08-22 14:30:00 -0500

    the main data structure is Boost::Graph, Added AVL-tree for quick-lookup

---
+++ src/Edge.cpp
@@ -0,0 +1,9 @@
+//
+//  Edge.cpp
+//  treeSAT
+//
+//  Created by Adel Ahmadyan on 8/21/13.
+//  Copyright (c) 2013 Adel Ahmadyan. All rights reserved.
+//
+
+#include "Edge.h"
---
+++ src/Edge.h
@@ -0,0 +1,14 @@
+//
+//  Edge.h
+//  treeSAT
+//
+//  Created by Adel Ahmadyan on 8/21/13.
+//  Copyright (c) 2013 Adel Ahmadyan. All rights reserved.
+//
+
+#ifndef __treeSAT__Edge__
+#define __treeSAT__Edge__
+
+#include <iostream>
+
+#endif /* defined(__treeSAT__Edge__) */
